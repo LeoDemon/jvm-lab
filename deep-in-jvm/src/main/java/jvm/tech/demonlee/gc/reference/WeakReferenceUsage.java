d2868b21b95f21d6b7f66d2bf5ae0bcf4b64509e
Demon.Lee <jwkljh@aliyun.com>
2023-12-04 12:01:41 +0800

    test(deep-understanding-of-jvm): chapter 3.2, new test cases for weak reference queue

--- deep-in-jvm/src/main/java/jvm/tech/demonlee/gc/reference/WeakReferenceUsage.java
+++ deep-in-jvm/src/main/java/jvm/tech/demonlee/gc/reference/WeakReferenceUsage.java
@@ -1,6 +1,8 @@
 package jvm.tech.demonlee.gc.reference;
 
 import jvm.tech.demonlee.common.model.Student;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 
 import java.lang.ref.WeakReference;
 import java.util.Objects;
@@ -9,23 +11,19 @@ import java.util.Objects;
  * @author Demon.Lee
  * @date 2023-11-12 15:34
  */
+@Log4j2
 public class WeakReferenceUsage {
 
-    public static void main(String[] args) {
-        WeakReference<Student> weakRef = new WeakReference<>(new Student("10001", "LiLei"));
-        checkExists(weakRef);
-
-        System.gc();
-
-        checkExists(weakRef);
-    }
-
-    private static void checkExists(WeakReference<Student> weakRef) {
-        Student cacheStu = weakRef.get();
-        if (Objects.isNull(cacheStu)) {
-            System.out.println("stu is killed.");
+    @SneakyThrows
+    public void checkExists(WeakReference<Student> weakRef) {
+        Student stu = weakRef.get();
+        log.info("stu is enqueued to ref queue: {}, {}", weakRef.isEnqueued(), stu);
+        if (Objects.isNull(stu)) {
+            log.info("stu is killed.");
+            log.info("stu is enqueued to ref queue: {}", weakRef.isEnqueued());
             return;
         }
-        System.out.println("stu is alive: " + cacheStu);
+        log.info("stu is alive: " + stu);
+        log.info("stu is enqueued to ref queue: {}", weakRef.isEnqueued());
     }
 }
---
+++ deep-in-jvm/src/test/java/jvm/tech/demonlee/gc/reference/WeakReferenceUsageTest.java
@@ -0,0 +1,51 @@
+package jvm.tech.demonlee.gc.reference;
+
+import jvm.tech.demonlee.common.model.Student;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.Objects;
+
+/**
+ * @author Demon.Lee
+ * @date 2023-12-04 11:16
+ */
+@Log4j2
+public class WeakReferenceUsageTest {
+
+    @Test
+    void usage() {
+        WeakReference<Student> weakRef = new WeakReference<>(new Student("10001", "LiLei"));
+        usage(weakRef);
+    }
+
+    @Test
+    void usageWithReferenceQueue() throws InterruptedException {
+        ReferenceQueue<Student> referenceQueue = new ReferenceQueue<>();
+        WeakReference<Student> weakRef = new WeakReference<>(new Student("10001", "LiLei"), referenceQueue);
+        usage(weakRef);
+        usageWithReferenceQueue(referenceQueue);
+    }
+
+    private void usageWithReferenceQueue(ReferenceQueue<Student> referenceQueue) throws InterruptedException {
+        Reference<? extends Student> queueRef = referenceQueue.remove(3000);
+        log.info("get ref from queue: {}", queueRef);
+        if (Objects.nonNull(queueRef)) {
+            log.info("get ref.stu from queue: {}", queueRef.get());
+        }
+    }
+
+    private void usage(WeakReference<Student> weakRef) {
+        WeakReferenceUsage weakReferenceUsage = new WeakReferenceUsage();
+        weakReferenceUsage.checkExists(weakRef);
+
+        log.info("Suggest the garbage collector to initiate a collection...");
+        System.gc();
+
+        weakReferenceUsage.checkExists(weakRef);
+    }
+
+}
