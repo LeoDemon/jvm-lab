6e4edd67c5a4c9f3c91ff13ab7f9ee85021fe1ee
Demon.Lee <jwkljh@aliyun.com>
2024-12-15 20:54:31 +0800

    refactor: optimize JNI example codes

--- deep-in-jvm/src/main/java/foo_native.c
+++ deep-in-jvm/src/main/java/foo_native.c
@@ -5,11 +5,8 @@ JNIEXPORT jint JNICALL Java_jvm_tech_demonlee_jni_FooNative_bar
   (JNIEnv *env, jobject thisObject, jint i, jstring str) {
   // 将 jstring 转换为 C 的 char*
   const char *nativeString = (*env)->GetStringUTFChars(env, str, NULL);
-  if (nativeString == NULL) {
-      return -1;
-  }
   printf("+++++++++Hello, %s, %d\n", nativeString, i);
-  jint result = 10*i*i;
   (*env)->ReleaseStringUTFChars(env, str, nativeString);
+  jint result = 10*i*i;
   return result;
 }
